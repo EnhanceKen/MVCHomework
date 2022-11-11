0674e4b8b578f05924abecc0b8f3e4a2d83897fb
Consultant <consultant@Syss-MacBook-Pro-4.local>
2022-11-11 09:27:42 -0500

    init commit

---
+++ MVCPaper.playground/Contents.swift
@@ -0,0 +1,9 @@
+/*
+ A MVC is a patter in software design commonly used to implement user interfaces, data, and controlling logic. The model view controller emphasises a separation between the software’s business logic and display. This separation provides a better division of labor and improved maintenance. There are three main parts of the MVC software design patter are the model, the view and the controller.
+
+     The model defines what data the app should contain. If the state of the data in the model changes, then the model will notify the view, so the display can change as needed, or it will notify the controller if the different logic is needed to control the updated view. The view defines how the app’s data should be displayed. The controller contains logic that updates the model and/or view in response to input from the users of the app. An app could have input forms and buttons that allow us to add or delete items. These actions require the model to be updates, so the input is sent to the controller, which then manipulates the model as appropriate and then sends updated data to the view.
+
+     There are some pro’s and cons to MVC pattern. Faster development process, Ability to provide multiple views, support for asynchronous technique, SEO friendly development platform, and the ability to modify without effecting the entire model are some of the pros. Unfortunately MVC patterns are high in complexity when it comes to app development. They also aren’t suitable for for small applications which has adverse effect in the application’s performance and design.
+
+ */
+
